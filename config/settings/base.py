906b41837f36648a3b87997094042193035e01f7
Matheus <matheusthayna082@gmail.com>
2026-03-16 08:50:27 -0300

    config: switch to render deploy

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-producti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=config('DJANGO_DEBUG', default=False, cast=bool), cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com').split(',')
 
 # Application definition
 DJANGO_APPS = [
